abe390eb6e1b24d4278fc0fb355d3e2bf842854d
Erich Ess <erich@jet.com>
2016-03-26 17:34:52 -0400

    added binomial heap.

--- purely_functional_data_structures/Chapter3.fs
+++ purely_functional_data_structures/Chapter3.fs
@@ -48,3 +48,60 @@ module LeftistHeaps =
 
     let heapList = l |> List.map (insert Empty)
     execute heapList
+
+module BinomialHeap =
+  type Tree<'a> = Node of int * 'a * (Tree<'a> list)
+
+  let root (Node(_, x, _)) = x
+  
+  let link t1 t2 =
+    let (Node(r, x1, c1)) = t1
+    let (Node(_, x2, c2)) = t2
+    if x1 <= x2
+    then Node(r + 1, x1, t2::c1)
+    else Node(r+ 1, x2, t1::c2)
+
+  type Heap<'a> = Tree<'a> list
+
+  let rank (Node(r, _, _)) = r
+
+  let rec insertTree t tl =
+    (t,tl)
+    |> function
+    | (t, []) -> [t]
+    | (t, t'::tl') -> 
+      if rank t < rank t'
+      then t::tl
+      else insertTree (link t t') tl'
+
+  let rec merge t1 t2 =
+    match (t1, t2) with
+    | (t1, []) -> t1
+    | ([], t2) -> t2
+    | (h1::t1', h2::t2') ->
+      if rank h1 < rank h2
+      then h1 :: (merge t1' t2)
+      elif rank h2 < rank h1
+      then h2 :: (merge t1 t2')
+      else insertTree (link h1 h2) (merge t1' t2')
+
+  let rec removeMinTree = function
+    | [] -> failwith "Empty"
+    | [t] -> (t, [])
+    | t::ts ->
+      let (t', ts') = removeMinTree ts
+      if root t <= root t'
+      then (t, ts)
+      else (t', t::ts)
+
+  let findMin t =
+    removeMinTree t
+    |> fst
+    |> root
+
+  // here's a point free version of the same function.
+  let findMin'<'a> = removeMinTree >> fst >> root
+
+  let deleteMin ts =
+    let (Node(_, x, ts1), ts2) = removeMinTree ts
+    merge (List.rev ts1) ts2
